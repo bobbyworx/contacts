5ac7157b9fe0e6ce744269ab8f8ca2797ebe5923
bogdan.popa <bogdan.popa.a@gmail.com>
2015-05-04 18:47:42 +0300

    Validations and error messages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,10 @@
 class Contact < ActiveRecord::Base
 
+  validates :first_name, uniqueness: true
+  validates :last_name, uniqueness: true
+  validates_format_of :email,
+    with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/, :unless => lambda {|u| u.email.nil? }
+
   def name
     [self.first_name, self.last_name].compact.join " "
   end
