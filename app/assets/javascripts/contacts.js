d375a93a48fdede261c74ba4ab1834c0136478d1
bogdan.popa <bogdan.popa.a@gmail.com>
2015-05-06 10:44:30 +0300

    Disable turbolink to fix js functions load

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {
+var select2Gem = function() {
   $("#contact_group_ids").select2({ width: 'resolve', placeholder: "Search group name", allowClear: true}
   );
-});
+};
+
+$(document).ready(select2Gem);
+$(document).on('page:load', select2Gem);
--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -5,8 +5,10 @@
 //       $(".group-contacts").find('.contacts-expandable').slideToggle(400);
 //     });
 
-$(document).ready(function() {
+var select2Gem = function() {
   $("#group_contact_ids").select2({ width: 'resolve', placeholder: "Search contact name", allowClear: true}
   );
-});
+};
 
+$(document).ready(select2Gem);
+$(document).on('page:load', select2Gem);
