365a6462e2376d1f25b3277826d2b8504908a955
bogdan.popa <bogdan.popa.a@gmail.com>
2015-05-06 12:38:55 +0300

    Custom routes for future password_reset feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-
   root 'contacts#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :users
+
     resources :contacts
+
     resource :session
+
     resources :groups
+
+  # get 'password_resets/new'
+  # get '/set/:password_reset_token' => 'password_resets#edit', as: 'reset_short'
+  # patch '/update/:password_reset_token' => 'password_resets#update', as: 'update_password'
 end
