d90f265ee6726bba1018df567f4233f54d41adde
bogdan.popa <bogdan.popa.a@gmail.com>
2015-05-06 11:35:14 +0300

    Add phone to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,6 +9,7 @@ class Contact < ActiveRecord::Base
   validates :last_name, presence: true, uniqueness: { scope: :user_id }
   validates_format_of :email,
     with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/, :unless => lambda {|u| u.email.nil? }
+  validates :phone, allow_blank: true, numericality: true
 
   def name
     [self.first_name, self.last_name].compact.join " "
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505112041) do
+ActiveRecord::Schema.define(version: 20150506083304) do
 
   create_table "contacts", force: true do |t|
     t.string   "first_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150505112041) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "phone"
   end
 
   create_table "group_contacts", force: true do |t|
